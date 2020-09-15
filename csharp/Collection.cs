e7d1f0540ee66832e7e78693ee24dc444413e756
Sarita-Jayswal <Sarita.jaiswal2073@gmail.com>
2020-09-15 12:53:40 +0545

    Added Today's Work

---
+++ csharp/Collection.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+namespace LearnCollections
+{
+    public class Collections
+    {
+        void LearnLists()
+        {
+            List<byte> ages = new List<byte> {2,3};
+            ages.Add(1);
+            ages.Add(10);
+            ages.Add(100);
+            ages.Add(255);
+            
+List<Country> countries = new List<Country>();
+
+countries.Add(new Country());
+        }
+       public void LearnDictionary()
+        {
+            var countryCapitals = new Dictionary<string, string>
+            {
+["Nepal"] = "Kathmandu",
+["India"] = "Delhi"
+            };
+           foreach (var country in countryCapitals)
+           {
+               Console.WriteLine($"Country: {country.Key} Capital City: {country.Value}");
+           }
+            
+        }
+    }
+    public class Country
+    {
+        public string Name {get; set;}
+    }
+}
--- csharp/Methods.cs
+++ csharp/Methods.cs
@@ -59,7 +59,7 @@ public class MethodTeacher{
  
 public class MethodTester 
  {
-     static void Main()
+     static void Main10()
     {
         MethodTeacher methodTeacher = new MethodTeacher();
         int[] numbers = {34,563,45,7,8,90,678,342,435412112,1,0,6,43,4};
